25046658f95a86863a7f4524fe747e040e72f478
Haoxuan Cui <haoxuan.cui@mullenmail.com>
2018-02-13 09:09:29 -0700

    tuesday fisrt

--- Inventory Thing/src/employee.java
+++ Inventory Thing/src/employee.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.ArrayList;
 import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,15 +6,40 @@ import java.io.IOException;
 import java.text.*;
 public class employee
 	{
-
+		static ArrayList<Game> stock = new ArrayList<Game>();// stock
+		private static int userChoice;
 		public static void main(String[] args)
 			{
-				static ArrayList<Game> stock = new ArrayList<Game>();// stock
+				
+				Scanner file = new Scanner(new File("HereItIs.txt"));
+				while(file.hasNextLine())
+					{
+						String list =file.nextLine();
+						String [] reallist= list.split(" ");
+						stock.add(new Game(reallist[0],reallist[1],reallist[2],reallist[3],reallist[4],reallist[5],reallist[6]));
+					} 
+				
 				System.out.println("Hi, here is the stock for us.");
 				for(int i=0; i<stock.size();i++)
 					{
 						System.out.println(Games.roster.get(i).getName()+Games.roster.get(i).getSkus()+Games.roster.get(i).getConsole()+Games.roster.get(i).getWhole()+Games.roster.get(i).getSell());
 					}
+				
+				System.out.println("What do you want to do?");
+				System.out.println("1. Buy Stock");
+				System.out.println("2. Exit the website");
+				Scanner userInput = new Scanner(System.in);
+				userChoice = userInput.nextInt();
+				if(userChoice==1)
+					{
+						
+					}
+				else if(userChoice
+						==2)
+					{
+						System.out.println("Bye bye!!!");
+					}
+				
 			}
 
 	}
