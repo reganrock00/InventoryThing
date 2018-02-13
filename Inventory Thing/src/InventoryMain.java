d36fffc53d0c23a7b5066c91160ffee3885414fa
Regan Archibald <regan.archibald@mullenmail.com>
2018-02-13 09:07:35 -0700

    updated text file and made games

--- Inventory Thing/src/InventoryMain.java
+++ Inventory Thing/src/InventoryMain.java
@@ -1,11 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
 
 public class InventoryMain
 	{
 
 		public static void main(String[] args)
 			{
-
-				System.out.println("Haoxuan Cui");
+				ArrayList <Game> store = new ArrayList <Game>();
+				store.add(new Game("Monster Hunter: World",  01234, "XBOX",  20, 60));
+				store.add(new Game("Fortnite",  01235, "XBOX", 0, 60));
+				store.add(new Game("Destiny",  01236, "Playstation",  4, 20));
+				store.add(new Game("Hitman",  01237, "PC", 5, 10));
+				store.add(new Game("Madden",  01233, "Playstation", 1, 30));
+				store.add(new Game("Minecraft",  01232, "PC", 10, 9));
 			}
 
 	}
