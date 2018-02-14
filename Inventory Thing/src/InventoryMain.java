a0e8d1206deadd3df6e873449a6cd435c8b45a29
Jack Paul <jackrpaul13@gmail.com>
2018-02-14 08:39:49 -0700

    yeet

--- Inventory Thing/src/InventoryMain.java
+++ Inventory Thing/src/InventoryMain.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 
 public class InventoryMain
 	{
-
+		public static ArrayList <Game> store = new ArrayList <Game>();
 		public static void main(String[] args)
 			{
-				ArrayList <Game> store = new ArrayList <Game>();
+				
 				store.add(new Game("Monster Hunter: World",  01234, "XBOX",  20, 60));
 				store.add(new Game("Fortnite",  01235, "XBOX", 0, 60));
 				store.add(new Game("Destiny",  01236, "Playstation",  4, 20));
