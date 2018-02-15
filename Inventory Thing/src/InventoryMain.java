5d0756f78c4881c7fcc2b144f0de10577a23ac7f
Jack Paul <jackrpaul13@gmail.com>
2018-02-15 12:05:06 -0700

    stuff is done

--- Inventory Thing/src/InventoryMain.java
+++ Inventory Thing/src/InventoryMain.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class InventoryMain
 	{
@@ -8,13 +8,41 @@ public class InventoryMain
 			{
 				fillArray();
 				TextWriter.refresh();
-				Buyer.buy();
+				System.out.println("Are you a buyer or an employee?");
+				System.out.println("1) Buyer");
+				System.out.println("2) Employee");
+				System.out.println();
+				try
+					{
+					Scanner userInput = new Scanner(System.in);
+					int userChoice = userInput.nextInt();
+					if(userChoice > 2 || userChoice < 1)
+						{
+						System.out.println("That's not a valid answer.");
+						System.out.println();
+						InventoryMain.main(args);
+						}
+					else if(userChoice == 1)
+						{
+						Buyer.buy();
+						}
+					else
+						{
+						//Employee.main(args) throws FileNotFoundException, UnsupportedEncodingException;
+						}
+					}
+				catch(InputMismatchException nice)
+					{
+						System.out.println("That's not a valid answer.");
+						System.out.println();
+						InventoryMain.main(args);
+					}
 			}
 		
 		public static void fillArray()
 			{
 				
-				store.add(new Game("Monster Hunter: World",  "01234", "XBOX",  "20", "60", "10"));
+				store.add(new Game("Monster Hunter",  "01234", "XBOX",  "20", "60", "10"));
 				store.add(new Game("Fortnite",  "01235", "XBOX", "0", "60", "10"));
 				store.add(new Game("Destiny",  "01236", "Playstation",  "4", "20", "10"));
 				store.add(new Game("Hitman",  "01237", "PC", "5", "10", "10"));
